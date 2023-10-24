b73321f0a7b8939d726319dec2dac0a530e936c1
dmitry-indenbom <dmitry.indenbom@yandex.ru>
2023-10-24 22:57:48 +0300

    First steps

--- hw1/protocol.py
+++ hw1/protocol.py
@@ -18,9 +18,21 @@ class UDPBasedProtocol:
 class MyTCPProtocol(UDPBasedProtocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.mtu = 1500
+        self.window_size = self.mtu * 10
 
     def send(self, data: bytes):
-        return self.sendto(data)
+        sent_bytes = 0
+        for i in range(self.mtu, len(data), self.mtu):
+            sent_bytes += self.sendto(data[:i])
+            if i != len(data):
+                data = data[i:]
+            else:
+                data = bytes()
+            # TODO: check receiving message if windows size is achieved
+        if data:
+            sent_bytes += self.sendto(data)
+        return sent_bytes
 
     def recv(self, n: int):
         return self.recvfrom(n)
